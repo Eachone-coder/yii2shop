e2f683aef119fbd08af049af9195cc0bbaaff735
Eachone <30427893+Eachone-coder@users.noreply.github.com>
2018-01-10 23:20:59 +0800

    定时任务

---
+++ console/controllers/ViewsController.php
@@ -0,0 +1,28 @@
+<?php
+namespace console\controllers;
+
+use backend\models\Goods;
+use yii\console\Controller;
+
+class ViewsController extends Controller{
+    public function actionSynchronize($max){
+        //set_time_limit(0);
+        $redis=new \Redis();
+        $redis->open('127.0.0.1','6379');
+        /*
+        脚本
+        */
+        $id=1;
+        while($id<$max){
+            $views=$redis->get('views_'.$id);
+            echo iconv('utf-8','gbk',$views);
+            echo "\n";
+            if ($views){
+                Goods::updateAll(['view_times'=>$views],['id'=>$id]);
+            }
+            $id+=1;
+        }
+        echo iconv('utf-8','gbk','同步完成').date('Y-m-d H:i:s');
+        echo "\n";
+    }
+}
